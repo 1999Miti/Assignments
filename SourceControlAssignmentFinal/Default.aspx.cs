dc8da724945fb669e2571c862fc04c655ee9b4ef
1999Miti <73311221+1999Miti@users.noreply.github.com>
2020-12-22 13:16:19 +0530

    Add files via upload

---
+++ SourceControlAssignmentFinal/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using WebApplication8.Models;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    protected void Page_Load(object sender,EventArgs e)
+    {
+        log4net.Config.XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/log4net.config")));
+
+    }
+    protected void ClickNLog(object sender,EventArgs e)
+    {
+        log.Warn("just clicked");
+    }
+}
